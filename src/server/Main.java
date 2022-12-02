88edbb781893e9d1021616e263f34d56e96c64f2
Lertos <deetasticgaming@gmail.com>
2022-12-01 20:59:06 -0400

    Fixing weird Main class bug

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,5 +1,19 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try(ServerSocket serverSocket = new ServerSocket(5000)) {
+            while(true) {
+                Socket socket = serverSocket.accept();
+                Client client = new Client(socket);
+                client.start();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
